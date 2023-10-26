e79d71f2fff9635bd3e7adfea4ff5169d76f8905
deveshidwivedi <deveshigurgaon@gmail.com>
2023-10-26 07:18:22 +0530

    updated files

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,18 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try{
+    const r=await axios.post(
+        "https://api.chatengine.io/users/",
+        { username, secret, email, first_name, last_name },
+        {headers:{"Private-Key":"813ae439-a99e-4ff0-aee4-5c97546f6b9a"}}
+    );
+    return res.status(res.status).json(res.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+
 });
 
 app.listen(3001);
